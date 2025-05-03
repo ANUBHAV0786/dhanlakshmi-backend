e8d55f720026681060ae6ad9f4e2869035540213
Anubhav Sharma <134250783+ANUBHAV0786@users.noreply.github.com>
2025-05-03 10:53:15 +0530

    trying re deploy

--- backend/settings.py
+++ backend/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://classy-unicorn-ee7468.netlify.app",  # replace with your custom domain later
 ]
